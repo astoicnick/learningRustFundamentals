7a9749c79c1ba7733a8ca236d7369232cf11a914
Nick Perry <nicholasperrypro@gmail.com>
2020-05-10 12:22:06 -0400

    Enums

--- src/enums.rs
+++ src/enums.rs
@@ -5,6 +5,16 @@ enum Movement {
     Right,
 }
 
-fn move_avatar(m: Movement) {}
+fn move_avatar(m: Movement) {
+    // match, similar to switch in c#
+    match m {
+        Movement::Up => println!("Avatar Moved Up"),
+        Movement::Down => println!("Avatar Moved Down"),
+        Movement::Left => println!("Avatar Moved Left"),
+        Movement::Right => println!("Avatar Moved Right"),
+    }
+}
 
-pub fn run() {}
+pub fn run() {
+    let avatar1 = Movement::Left;
+}
